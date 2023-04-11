f55decd9c49098ee6a7b2d3d92846a242930ce6d
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2023-04-11 10:27:34 +0200

    Starts adding music

--- Game212cpp/Game212cpp/01_hello_SDL.cpp
+++ Game212cpp/Game212cpp/01_hello_SDL.cpp
@@ -4,6 +4,7 @@ and may not be redistributed without written permission.*/
 //Using SDL, SDL_image, standard IO, and strings
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_mixer.h>
 #include <stdio.h>
 #include <string>
 
