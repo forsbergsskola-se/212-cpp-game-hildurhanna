b82b223f3d8a748b08b31704e7c4768eca9bd343
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2023-04-13 20:36:54 +0200

    Adds code and header

--- Game212cpp/Game212cpp/Texture.cpp
+++ Game212cpp/Game212cpp/Texture.cpp
@@ -1,5 +1,7 @@
 #include "Texture.h"
 
+#include <SDL_image.h>
+
 Texture::Texture()
 {
 }
@@ -10,7 +12,43 @@ Texture::~Texture()
 
 bool Texture::loadFromFile(std::string path)
 {
-    return false;
+	//Get rid of preexisting texture
+	free();
+
+	//The final texture
+	SDL_Texture* newTexture = nullptr;
+
+	//Load image at specified path
+	SDL_Surface* loadedSurface = IMG_Load(path.c_str());
+	if (loadedSurface == nullptr)
+	{
+		printf("Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError());
+	}
+	else
+	{
+		//Color key image
+		SDL_SetColorKey(loadedSurface, SDL_TRUE, SDL_MapRGB(loadedSurface->format, 0, 0xFF, 0xFF));
+
+		//Create texture from surface pixels
+		newTexture = SDL_CreateTextureFromSurface(gRenderer, loadedSurface);
+		if (newTexture == nullptr)
+		{
+			printf("Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError());
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = loadedSurface->w;
+			mHeight = loadedSurface->h;
+		}
+
+		//Get rid of old loaded surface
+		SDL_FreeSurface(loadedSurface);
+	}
+
+	//Return success
+	mTexture = newTexture;
+	return mTexture != nullptr;
 }
 
 bool Texture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
