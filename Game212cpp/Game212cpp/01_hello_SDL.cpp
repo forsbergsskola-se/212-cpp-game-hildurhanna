2586c7e1c680625648980fed92d35d9d8e506a56
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2023-04-13 10:08:37 +0200

    Adds ttf, state and little reconstruction
    I know Marc, there should never be a 'add' but I was in the flow!

--- Game212cpp/Game212cpp/01_hello_SDL.cpp
+++ Game212cpp/Game212cpp/01_hello_SDL.cpp
@@ -1,17 +1,38 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2022)
-and may not be redistributed without written permission.*/
+
 
 //Using SDL, SDL_image, standard IO, and strings
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_ttf.h>
 #include <SDL_mixer.h>
 #include <stdio.h>
 #include <string>
 
-//Screen dimension constants
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+enum State 
+{
+	MENU_STATE = 0,
+	GAME_STATE,
+	END_STATE
+};
 
+//Screen dimension constants
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
+int CurrentState = State::MENU_STATE;
+int foxPosX = 0;
+int foxPosY = 0;
+
+float timer = 0.0f;
+float deltaTime = 0.0f;
+float currentTime = 0.0f;
+float lastTime = 0.0f;
+float lifetime = 0.0f;
+float sinus = 0.0f;
+float cosinus = 0.0f;
+
+bool running = true;
+bool timerIncrement = false;
 
 //Texture wrapper class
 class LTexture
@@ -25,6 +46,9 @@ public:
 
 	//Loads image at specified path
 	bool loadFromFile(std::string path);
+
+	//Creates image from font string
+	bool loadFromRenderedText(std::string textureText, SDL_Color textColor);
 
 	//Deallocates texture
 	void free();
@@ -54,8 +78,20 @@ bool loadMedia();
 //Frees media and shuts down SDL
 void close();
 
+void resetTimer();
+
+
 //The window we'll be rendering to
 SDL_Window* gWindow = nullptr;
+
+//Globally used font
+TTF_Font* gFont = nullptr;
+
+//Rendered  fonttexture
+LTexture gMenuInfoTexture;
+LTexture gStartTexture;
+LTexture gEndTexture;
+
 
 //The window renderer
 SDL_Renderer* gRenderer = nullptr;
@@ -63,13 +99,14 @@ SDL_Renderer* gRenderer = nullptr;
 //Scene textures
 LTexture gFoxTexture;
 LTexture gNatureBackgroundTexture;
+LTexture gEndFoxLeftTopTexture;
+LTexture gEndFoxRightBottomTexture;
 
 //The music that will be played
 Mix_Music *gMusic = nullptr;
 
 //Check if the music is playing
 bool gIsMusicPlaying = false;
-
 
 LTexture::LTexture()
 {
@@ -163,6 +200,40 @@ int LTexture::getHeight()
 	return mHeight;
 }
 
+bool LTexture::loadFromRenderedText(std::string textureText, SDL_Color textColor)
+{
+	//Get rid of preexisting texture
+	free();
+
+	//Render text surface
+	SDL_Surface* textSurface = TTF_RenderText_Solid(gFont, textureText.c_str(), textColor);
+	if (textSurface == NULL)
+	{
+		printf("Unable to render text surface! SDL_ttf Error: %s\n", TTF_GetError());
+	}
+	else
+	{
+		//Create texture from surface pixels
+		mTexture = SDL_CreateTextureFromSurface(gRenderer, textSurface);
+		if (mTexture == NULL)
+		{
+			printf("Unable to create texture from rendered text! SDL Error: %s\n", SDL_GetError());
+		}
+		else
+		{
+			//Get image dimensions
+			mWidth = textSurface->w;
+			mHeight = textSurface->h;
+		}
+
+		//Get rid of old surface
+		SDL_FreeSurface(textSurface);
+	}
+
+	//Return success
+	return mTexture != nullptr;
+}
+
 bool init()
 {
 	//Initialization flag
@@ -211,6 +282,13 @@ bool init()
 					success = false;
 				}
 
+				//Initialize SDL_ttf
+				if (TTF_Init() == -1)
+				{
+					printf("SDL_ttf could not initialize! SDL_ttf Error: %s\n", TTF_GetError());
+					success = false;
+				}
+
 				//Initialize SDL_mixer
 				if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)
 				{
@@ -229,8 +307,26 @@ bool loadMedia()
 	//Loading success flag
 	bool success = true;
 
+	//Open the font
+	gFont = TTF_OpenFont("Font/Boba_Cups.ttf", 28);
+	if (gFont == nullptr)
+	{
+		printf("Failed to load lazy font! SDL_ttf Error: %s\n", TTF_GetError());
+		success = false;
+	}
+	else
+	{
+		//Render text
+		SDL_Color textColor = { 0, 0, 0 };
+		if (!gMenuInfoTexture.loadFromRenderedText("Start the game with a mouse click!", textColor))
+		{
+			printf("Failed to render text texture!\n");
+			success = false;
+		}
+	}
+
 	//Load music
-	gMusic = Mix_LoadMUS("Sound_effects_and_music/beat.wav");
+	gMusic = Mix_LoadMUS("Music/What_Does_The_Fox_Say.ogg");
 	if (gMusic == nullptr)
 	{
 		printf("Failed to load beat music! SDL_mixer Error: %s\n", Mix_GetError());
@@ -240,14 +336,31 @@ bool loadMedia()
 	//Load Fox texture
 	if (!gFoxTexture.loadFromFile("Forest_Spirits_Image/foxcartoon.png"))
 	{
-		printf("Failed to load Fox texture image!\n");
+		printf("Failed to load foxcartoon.png texture image!\n");
 		success = false;
 	}
+
+	foxPosX = SCREEN_WIDTH	/ 2;
+	foxPosY = SCREEN_HEIGHT	/ 2;
 
 	//Load background texture
 	if (!gNatureBackgroundTexture.loadFromFile("Background_Image/nature_background.png"))
 	{
 		printf("Failed to load background texture image!\n");
+		success = false;
+	}
+
+	//Load background texture
+	if (!gEndFoxLeftTopTexture.loadFromFile("Forest_Spirits_Image/fox2.png"))
+	{
+		printf("Failed to load fox.png texture image!\n");
+		success = false;
+	}
+
+	//Load background texture
+	if (!gEndFoxRightBottomTexture.loadFromFile("Forest_Spirits_Image/fox.png"))
+	{
+		printf("Failed to load fox.png texture image!\n");
 		success = false;
 	}
 
@@ -259,6 +372,10 @@ void close()
 	//Free loaded images
 	gFoxTexture.free();
 	gNatureBackgroundTexture.free();
+
+	//Free global font
+	TTF_CloseFont(gFont);
+	gFont = NULL;
 
 	//Free the music
 	Mix_FreeMusic(gMusic);
@@ -272,14 +389,219 @@ void close()
 
 	//Quit SDL subsystems
 	Mix_Quit();
+	TTF_Quit();
 	IMG_Quit();
 	SDL_Quit();
 }
 
+void HandleEvents()
+{
+	SDL_Event e;
+
+	while (SDL_PollEvent(&e) != 0)
+	{
+		switch (e.type)
+		{
+			case SDL_QUIT:
+			{
+				running = false;
+
+				break;
+			}
+
+			case SDL_MOUSEBUTTONDOWN:
+			{
+				if (CurrentState == State::MENU_STATE)
+				{
+					int mouseX, mouseY;
+					SDL_GetMouseState(&mouseX, &mouseY);
+
+					//Todo switch to gamestate when button is pressed
+
+					CurrentState = State::GAME_STATE;
+					resetTimer();
+
+					break;
+				}
+
+				else if (CurrentState == State::GAME_STATE)
+				{
+					int mouseX, mouseY;
+					SDL_GetMouseState(&mouseX, &mouseY);
+
+					const SDL_FPoint mousePosition = {(float)mouseX, (float)mouseY};
+					const SDL_FRect foxQuad = {(float)foxPosX, (float)foxPosY, (float)gFoxTexture.getWidth(), (float)gFoxTexture.getHeight()};
+
+					if(SDL_PointInFRect(&mousePosition, &foxQuad))
+					{
+						if (!gIsMusicPlaying)
+						{
+							Mix_PlayMusic(gMusic, -1);
+							gIsMusicPlaying = true;
+						//	printf("The fox is being clicked on!\n");
+
+							//Start the timer here
+							timerIncrement = true;
+						}
+					}
+					
+
+					break;
+				}
+
+				else if (CurrentState == State::END_STATE)
+				{
+					// TODO: maybe go back to the menu state
+					// if you want to go to the menu state, do this:
+					CurrentState = State::MENU_STATE;
+
+					break;
+				}
+
+				break;
+			}
+
+			case SDL_MOUSEBUTTONUP:
+			{
+				if (CurrentState == State::MENU_STATE)
+				{
+					// Do something in the menu when the mouse button is released?
+					// Do that here
+
+					break;
+				}
+
+				else if (CurrentState == State::GAME_STATE)
+				{
+					if (gIsMusicPlaying)
+					{
+						Mix_HaltMusic();
+						gIsMusicPlaying = false;
+					//	printf("The fox has been released!\n");
+
+						// Generate the end texture
+						SDL_Color textColor = { 0, 0, 0 };
+						gEndTexture.loadFromRenderedText("You managed to hold the mouse pointer for " + std::to_string((int)timer) + " seconds", textColor);
+
+						//Stop the timer
+						timerIncrement = false;
+
+						CurrentState = State::END_STATE;
+					}
+
+					break;
+				}
+
+				else if (CurrentState == State::END_STATE)
+				{
+					// Do something in the end state when the mouse button is released?
+					// Do that here
+
+
+					break;
+				}
+
+				break;
+			}
+
+			default:
+				break;
+		}
+	}
+}
+
+void resetTimer()
+{
+	timer = 0.0f;
+	timerIncrement = false;
+}
+
+void Update() 
+{
+	lastTime = currentTime;
+	currentTime = SDL_GetTicks();
+
+	deltaTime = (currentTime - lastTime) * 0.001;
+
+	if (CurrentState == State::GAME_STATE)
+	{
+		if (timerIncrement)
+			timer += deltaTime;
+
+	//	printf("timer: %f\n", timer);
+	}
+
+	else if (CurrentState == State::END_STATE)
+	{
+		lifetime += deltaTime;
+
+		sinus = sinf(lifetime * 4.0f);
+		cosinus = cosf(lifetime * 4.0f);
+
+		printf("sinus: %f\n", sinus);
+	}
+}
+
+void Render()
+{
+	SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
+	SDL_RenderClear(gRenderer);
+
+	switch (CurrentState)
+	{
+		// Render the menu state
+		case State::MENU_STATE:
+		{
+			// TODO: render menu stuff
+
+			//Render current frame
+			gMenuInfoTexture.render((SCREEN_WIDTH - gMenuInfoTexture.getWidth()) / 2, (SCREEN_HEIGHT - gMenuInfoTexture.getHeight()) / 2);
+
+
+
+			break;
+		}
+
+		// Render the game state
+		case State::GAME_STATE:
+		{
+			//Render background texture to screen
+			gNatureBackgroundTexture.render(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+			//Render Fox to the screen
+			gFoxTexture.render(foxPosX, foxPosY);
+
+		//	SDL_Rect FoxQuad = {foxPosX, foxPosY, gFoxTexture.getWidth(), gFoxTexture.getHeight()};
+		//	SDL_SetRenderDrawColor(gRenderer, 200, 0, 0, 255);
+		//	SDL_RenderFillRect(gRenderer, &FoxQuad);
+
+			break;
+		}
+
+		// Render the game state
+		case State::END_STATE:
+		{
+			// Render the stuff for the end state (some text etc?)
+
+			gEndTexture.render((SCREEN_WIDTH - gEndTexture.getWidth()) / 2, (SCREEN_HEIGHT - gEndTexture.getHeight()) / 2);
+
+			const Uint32 flip = SDL_FLIP_HORIZONTAL | SDL_FLIP_VERTICAL;
+
+			gEndFoxLeftTopTexture.render(-gEndFoxLeftTopTexture.getWidth() + (cosinus * 300.0f), -gEndFoxLeftTopTexture.getHeight() + (cosinus * 300.0f));
+			gEndFoxRightBottomTexture.render(SCREEN_WIDTH - (sinus * 300.0f), SCREEN_HEIGHT - (sinus * 300.0f));
+
+			break;
+		}
+
+		default:
+			break;
+	}
+
+	SDL_RenderPresent(gRenderer);
+}
+
 int main(int argc, char* args[])
 {
-	int foxPosX = 200;
-	int foxPosY = 300;
 	//Start up SDL and create window
 	if (!init())
 	{
@@ -287,70 +609,23 @@ int main(int argc, char* args[])
 	}
 	else
 	{
+		currentTime = SDL_GetTicks();
+		lastTime = SDL_GetTicks();
+
 		//Load media
 		if (!loadMedia())
 		{
 			printf("Failed to load media!\n");
 		}
+
 		else
 		{
-			//Main loop flag
-			bool quit = false;
-
-			//Event handler
-			SDL_Event e;
-
 			//While application is running
-			while (!quit)
+			while (running)
 			{
-				//Handle events on queue
-				while (SDL_PollEvent(&e) != 0)
-				{
-					//User requests quit
-					if (e.type == SDL_QUIT)
-					{
-						quit = true;
-					}
-
-					// Handle mouse events
-					else if (e.type == SDL_MOUSEBUTTONDOWN)
-					{
-						int mouseX, mouseY;
-						SDL_GetMouseState(&mouseX, &mouseY);
-						if (mouseX >= foxPosX && mouseX < foxPosX + gFoxTexture.getWidth() &&
-							mouseY >= foxPosX && mouseY < foxPosY + gFoxTexture.getHeight())
-						{
-							if (!gIsMusicPlaying)
-							{
-								Mix_PlayMusic(gMusic, -1);
-								gIsMusicPlaying = true;
-								printf("The fox is being clicked on!\n");
-							}
-						}
-					}
-					else if (e.type == SDL_MOUSEBUTTONUP)
-					{
-						if (gIsMusicPlaying)
-						{
-							Mix_HaltMusic();
-							gIsMusicPlaying = false;
-							printf("The fox has been released!\n");
-						}
-					}
-				}
-
-				//Clear screen
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-				SDL_RenderClear(gRenderer);
-
-				//Render background texture to screen
-				gNatureBackgroundTexture.render(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-				//Render Foo' to the screen
-				gFoxTexture.render(foxPosX, foxPosY, SCREEN_WIDTH / 3, SCREEN_HEIGHT / 3);
-
-				//Update screen
-				SDL_RenderPresent(gRenderer);
+				HandleEvents();
+				Update();
+				Render();
 			}
 		}
 	}
