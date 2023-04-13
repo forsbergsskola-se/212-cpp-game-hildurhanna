19e88ac98d322e12c04e020a5bf9aa585bb15a61
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2023-04-13 20:14:14 +0200

    Moves code to Application.h

--- Game212cpp/Game212cpp/Application.cpp
+++ Game212cpp/Game212cpp/Application.cpp
@@ -1,5 +1,9 @@
 #include "Application.h"
 
+//Screen dimension constants
+const int SCREEN_WIDTH = 1280;
+const int SCREEN_HEIGHT = 720;
+
 Application::Application()
 {
 }
@@ -10,6 +14,13 @@ Application::~Application()
 
 bool Application::init()
 {
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+	{
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
 	//Create window
 	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	if (!window)
@@ -31,6 +42,13 @@ bool Application::init()
 
 void Application::run()
 {
+	//While application is running
+	while (running)
+	{
+		handleEvents();
+		update();
+		render();
+	}
 }
 
 void Application::close()
@@ -39,6 +57,23 @@ void Application::close()
 
 void Application::handleEvents()
 {
+	SDL_Event e;
+
+	while (SDL_PollEvent(&e) != 0)
+	{
+		switch (e.type)
+		{
+			case SDL_QUIT:
+			{
+				running = false;
+
+				break;
+			}
+
+			default:
+				break;
+		}
+	}
 }
 
 void Application::update()
