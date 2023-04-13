76ac279c875ae0ba4b2c6e47e1dd47610bcdeda7
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2023-04-13 19:50:45 +0200

    Adds functionality to Application

--- Game212cpp/Game212cpp/Application.cpp
+++ Game212cpp/Game212cpp/Application.cpp
@@ -1,1 +1,50 @@
 #include "Application.h"
+
+Application::Application()
+{
+}
+
+Application::~Application()
+{
+}
+
+bool Application::init()
+{
+	//Create window
+	window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (!window)
+	{
+		printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	//Create renderer for window
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if (renderer == nullptr)
+	{
+		printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	return true;
+}
+
+void Application::run()
+{
+}
+
+void Application::close()
+{
+}
+
+void Application::handleEvents()
+{
+}
+
+void Application::update()
+{
+}
+
+void Application::render()
+{
+}
--- Game212cpp/Game212cpp/Application.h
+++ Game212cpp/Game212cpp/Application.h
@@ -1,6 +1,31 @@
 #pragma once
-class Application
+
+#include "Singleton.h"
+
+#include <SDL.h>
+
+class Application : public CSingleton<Application>
 {
+public:
+
+	 Application();
+	~Application();
+
+	bool init();
+	void run();
+	void close();
+
+	void handleEvents();
+	void update();
+	void render();
+
+private:
+
+	//The window we'll be rendering to
+	SDL_Window* gWindow = nullptr;
+
+	//The window renderer
+	SDL_Renderer* gRenderer = nullptr;
 
 };
 
