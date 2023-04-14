2761975cdd932cb33ea12299927d27208a5787a9
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2023-04-14 09:18:54 +0200

    Gives update argument for deltaTime

--- Game212cpp/Game212cpp/State.h
+++ Game212cpp/Game212cpp/State.h
@@ -1,12 +1,14 @@
 #pragma once
 
+#include "Texture.h"
+
 class State
 {
 public:
 
 	State() {}
 	virtual ~State() {}
-	virtual void update() = 0;
+	virtual void update(const float deltaTime) = 0;
 	virtual void render() = 0;
 	virtual void onButtonMouseDown() = 0;
 	virtual void onButtonMouseUp() = 0;
