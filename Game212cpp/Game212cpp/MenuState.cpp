8f27b632e1d65e9bc4a3d6bc58021f2590dab6f1
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2023-04-14 09:20:15 +0200

    Reforms MenuState to apply Application

--- Game212cpp/Game212cpp/MenuState.cpp
+++ Game212cpp/Game212cpp/MenuState.cpp
@@ -1,24 +1,31 @@
 #include "MenuState.h"
 
+#include "Application.h"
+
 MenuState::MenuState()
 {
+	SDL_Color textColor = { 0, 0, 0 };
+	menuInfoTexture.loadFromRenderedText("Start the game by clicking on the screen, or exit the game by the 'X' icon", textColor);
 }
 
 MenuState::~MenuState()
 {
+	menuInfoTexture.free();
 }
 
 
-void MenuState::update()
+void MenuState::update(const float deltaTime)
 {
 }
 
 void MenuState::render()
 {
+	menuInfoTexture.render((Application::GetInstance().getWindowWidth() - menuInfoTexture.getWidth()) / 2, (Application::GetInstance().getWindowHeight() - menuInfoTexture.getHeight()) / 2, menuInfoTexture.getWidth(), menuInfoTexture.getHeight());
 }
 
 void MenuState::onButtonMouseDown()
 {
+	Application::GetInstance().SetState(Application::GetInstance().GetGameState());
 }
 
 void MenuState::onButtonMouseUp()
