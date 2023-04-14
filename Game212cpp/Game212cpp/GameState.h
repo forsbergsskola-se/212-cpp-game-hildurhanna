f65aa3f4babcb9aa1504cfaa60cbcab1d635f086
hildurhanna <112477585+hildurhanna@users.noreply.github.com>
2023-04-14 09:22:00 +0200

    Adds argument to update and variables

--- Game212cpp/Game212cpp/GameState.h
+++ Game212cpp/Game212cpp/GameState.h
@@ -2,6 +2,8 @@
 
 #include "State.h"
 
+#include <SDL_mixer.h>
+
 class GameState : public State
 {
 public:
@@ -9,9 +11,25 @@ public:
 	GameState();
 	~GameState();
 
-	virtual void update() override;
+	virtual void update(const float deltaTime) override;
 	virtual void render() override;
 	virtual void onButtonMouseDown() override;
 	virtual void onButtonMouseUp() override;
-};
 
+private:
+	Texture foxTexture;
+	Texture natureBackgroundTexture;
+	Texture rulesTexture;
+
+	//The music that will be played
+	Mix_Music* music = nullptr;
+
+	int foxPosX = 0;
+	int foxPosY = 0;
+
+	float timer = 0.0f;
+
+	bool timerIncrement = false;
+	bool isMusicPlaying = false;
+
+};
